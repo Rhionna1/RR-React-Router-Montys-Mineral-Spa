accc3bde08f9940ad41312c0d0831be464aa9c26
Rhionna Landers <kasmiere@Rhionnas-Mac-mini.local>
2023-10-07 11:44:22 -0500

    react-router added pages with packages

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,6 +1,9 @@
 
 
-export default function Packages() {
+export default function Packages({ packages }) {
+    const packageDisplay = packages.map((item, i) => {
+        return <li key={i}>{item}</li>
+    })
 
     return (
         <div>
@@ -8,7 +11,9 @@ export default function Packages() {
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
                 </div>
-                
+                <ul>
+                    {packageDisplay}
+                </ul>
             </div>
         </div>
     )
